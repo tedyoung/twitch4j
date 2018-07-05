51a000ed318dcceb0e49176f341ef0186a916664
Damian Staszewski <damian@stachuofficial.pl>
2018-07-05 13:06:24 +0200

    docs(annotation): removing `@Retention` from `@Unofficial` annotation

--- common/src/main/java/twitch4j/common/utils/Unofficial.java
+++ common/src/main/java/twitch4j/common/utils/Unofficial.java
@@ -1,8 +1,6 @@
 package twitch4j.common.utils;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 /**
  * The Unofficial Annotation marks unofficial api endpoints.
@@ -11,14 +9,13 @@ import java.lang.annotation.RetentionPolicy;
  *
  * @author Philipp Heuer [https://github.com/PhilippHeuer]
  * @version %I%, %G%
- * @since 1.0
- */
+ * @since 1.0 */
+
 @Documented
-@Retention(RetentionPolicy.SOURCE)
 public @interface Unofficial {
 
 	/**
 	 * Source of the unofficial endpoint
 	 */
-	String source() default "https://dev.twitch.tv/";
+	String value();
 }
